e2d5e52a43c209466a3099efb1187d84b0384672
Bihan Chakraborty <bhchakraborty001@gmail.com>
2022-02-23 22:04:58 +0530

    new problems

---
+++ Distinct_Binary_Strings.cpp
@@ -0,0 +1,43 @@
+// Author: Bihan Chakraborty
+// Linkedin: https://www.linkedin.com/in/bihan-chakraborty
+// Github: https://github.com/Bihan001
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define vi vector<int>
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(), a.end()
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+#define inputarr(arr, n) \
+    for (int i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (int i = 0; i < n; i++) cout << arr[i] << ' ';
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int cnt = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[i] != s[i - 1]) cnt++;
+    }
+    cout << cnt << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ String_Reverse.cpp
@@ -0,0 +1,46 @@
+// Author: Bihan Chakraborty
+// Linkedin: https://www.linkedin.com/in/bihan-chakraborty
+// Github: https://github.com/Bihan001
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define vi vector<int>
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(), a.end()
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+#define inputarr(arr, n) \
+    for (int i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (int i = 0; i < n; i++) cout << arr[i] << ' ';
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.length(), cnt = 0, i = 0, j = s.length() - 1;
+    while (i < n) {
+        while (i < n && s[i] != s[j]) {
+            i += 1;
+            cnt += 1;
+        }
+        i += 1;
+        j -= 1;
+    }
+    cout << cnt << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
